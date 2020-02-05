f7ebb0ff796d59b5c0f18a300c4a7bada52467ca
Maksym Dominichenko <maksym.dominichenko@jpmchase.com>
2020-02-05 15:15:30 -0500

    Added arguments; Build as EXE

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 Mouse Mover
 ===========
 
-Very simple application that automatically moves mouse in screen every 5 seconds.
-This is useful if you want to prevent computer to lock screen or run screen saver.
-This movement activity is suppressed for 30 seconds if user moves mouse manually.
+Very simple application that automatically moves mouse in the screen (every 5 seconds by default).
+This is useful if you want to prevent the computer to lock screen or run screen saver.
+This movement activity is suppressed for some time (30 seconds by default) if user moves mouse manually.
 
 Multi-screen configuration is supported.
 
-By default mouse is moving to the right direction by 5 pixels. 
+By default, mouse is moving to the right direction by 5 pixels. 
 In case if cursor reaches edge of current screen then this motion changes direction. 
+
+You may change default setting using command line arguments.
+To get help about supported arguments, run application with `-h` option.
 
 Build
 -----
@@ -22,9 +25,9 @@ Run
 
 To run this app you have to have installed JDK or JRE 8 or higher:
 
-    java -jar target/mouse-mover.jar
+    target/mouse-mover.exe
 
-To run app with logging just put path to [debug.properties](log/debug.properties) file 
+To run app with logging just put path to [debug.properties](log/debug.properties) file
 to `java.util.logging.config.file` Java system variable, e.g:
 
     java -Djava.util.logging.config.file=log/debug.properties -jar target/mouse-mover.jar 
--- src/main/java/com/domax/MouseMover.java
+++ src/main/java/com/domax/MouseMover.java
@@ -3,62 +3,96 @@ package com.domax;
 import java.awt.AWTException;
 import java.awt.MouseInfo;
 import java.awt.Point;
-import java.awt.PointerInfo;
-import java.awt.Rectangle;
 import java.awt.Robot;
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.stream.Stream;
+import lombok.extern.java.Log;
+import lombok.val;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
 
+@Log
 public class MouseMover {
 
-  private static final Logger LOG = Logger.getLogger(MouseMover.class.getName());
-
-  private static final int SHORT_DELAY = 5000;
-  private static final int LONG_DELAY = 30000;
-  private static final int MOVE_DIST = 5;
-
-  private static final List<Point> MOVES =
-      Arrays.asList(
-          new Point(MOVE_DIST, 0),
-          new Point(-MOVE_DIST, 0),
-          new Point(0, MOVE_DIST),
-          new Point(0, -MOVE_DIST));
-
   public static void main(String... args) throws AWTException {
-    final Robot robot = new Robot();
-    final Point oldLocation = new Point(-1, -1);
+    val parser =
+        ArgumentParsers.newFor(MouseMover.class.getSimpleName())
+            .build()
+            .defaultHelp(true)
+            .description("Very simple application that automatically moves mouse in screen");
+    val argShortDelay =
+        parser
+            .addArgument("-s", "--short-delay")
+            .type(Integer.class)
+            .setDefault(5)
+            .help("Short delay in seconds between moves");
+    val argLongDelay =
+        parser
+            .addArgument("-l", "--long-delay")
+            .type(Integer.class)
+            .setDefault(30)
+            .help("Long delay in seconds before first move after mouse inactivity");
+    val argDistance =
+        parser
+            .addArgument("-d", "--distance")
+            .type(Integer.class)
+            .setDefault(5)
+            .help("Distance in pixels of mouse moving");
+    int shortDelay, longDelay, distance;
+    try {
+      val ns = parser.parseArgs(args);
+      shortDelay = ns.getInt("short_delay");
+      longDelay = ns.getInt("long_delay");
+      distance = ns.getInt("distance");
+      if (shortDelay < 1)
+        throw new ArgumentParserException(
+            "Short delay cannot be less than 1", parser, argShortDelay);
+      if (longDelay < 1)
+        throw new ArgumentParserException("Long delay cannot be less than 1", parser, argLongDelay);
+      if (shortDelay > longDelay)
+        throw new ArgumentParserException("Short delay cannot be less than long one", parser);
+      if (distance == 0)
+        throw new ArgumentParserException("Distance cannot be 0", parser, argDistance);
+      log.info(
+          String.format(
+              "Short Delay: %d; Long Delay: %d; Distance: %d", shortDelay, longDelay, distance));
+    } catch (ArgumentParserException e) {
+      parser.handleError(e);
+      System.exit(1);
+      return;
+    }
+
+    val robot = new Robot();
+    val oldLocation = new Point(-1, -1);
     while (true) {
       try {
-        final PointerInfo pointerInfo = MouseInfo.getPointerInfo();
-        final Point location = new Point(pointerInfo.getLocation());
-        final Rectangle bounds = pointerInfo.getDevice().getDefaultConfiguration().getBounds();
+        val pointerInfo = MouseInfo.getPointerInfo();
+        val location = new Point(pointerInfo.getLocation());
+        val bounds = pointerInfo.getDevice().getDefaultConfiguration().getBounds();
         location.translate(-bounds.x, -bounds.y);
         if (location.x < 0) location.x *= -1;
         if (location.y < 0) location.y *= -1;
 
         if (location.equals(oldLocation)) {
-          final Point newLocation =
-              MOVES
-                  .stream()
+          val newLocation =
+              Stream.of(new Point(distance, 0), new Point(-distance, 0))
                   .map(d -> new Point(bounds.x + location.x + d.x, bounds.y + location.y + d.y))
                   .filter(bounds::contains)
                   .findAny()
                   .orElseThrow(() -> new IllegalStateException("Cannot compute new location"));
-
           robot.mouseMove(newLocation.x, newLocation.y);
-          LOG.fine("New " + newLocation + " in " + bounds);
+          log.fine("New " + newLocation + " in " + bounds);
           newLocation.translate(-bounds.x, -bounds.y);
           oldLocation.setLocation(newLocation);
-          Thread.sleep(SHORT_DELAY);
+          distance = -distance;
+          Thread.sleep(shortDelay * 1000);
         } else {
-          LOG.fine("Old " + location + " in " + bounds);
+          log.fine("Old " + location + " in " + bounds);
           oldLocation.setLocation(location);
-          Thread.sleep(LONG_DELAY);
+          Thread.sleep(longDelay * 1000);
         }
       } catch (Exception e) {
-        LOG.log(Level.SEVERE, e.getMessage(), e);
+        log.log(Level.SEVERE, e.getMessage(), e);
         break;
       }
     }
